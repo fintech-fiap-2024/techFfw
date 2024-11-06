ee37f1baacf1721af049946bb583ca94906e0784
pedromarquesini <pedromarquesini1410@gmail.com>
2024-11-05 22:44:20 -0300

    first interface

---
+++ src/main/java/br/com/fiap/ffw/techffw/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package br.com.fiap.ffw.techffw.dao;
+
+import br.com.fiap.ffw.techffw.exception.DBException;
+import br.com.fiap.ffw.techffw.model.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    void cadastrar(Usuario usuario) throws DBException;
+    void atualizar(Usuario usuario) throws DBException;
+    void remover(Usuario usuario) throws DBException;
+    Usuario buscarPorNome(String nome);
+    List<Usuario> listar();
+
+}
