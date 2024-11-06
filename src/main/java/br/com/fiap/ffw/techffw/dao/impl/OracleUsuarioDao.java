fd819f3077723e26e06b97307abc1453207147b6
pedromarquesini <pedromarquesini1410@gmail.com>
2024-11-06 15:45:23 -0300

    add: UsuarioDao - funcionando

--- src/main/java/br/com/fiap/ffw/techffw/dao/impl/OracleUsuarioDao.java
+++ src/main/java/br/com/fiap/ffw/techffw/dao/impl/OracleUsuarioDao.java
@@ -5,9 +5,8 @@ import br.com.fiap.ffw.techffw.dao.UsuarioDao;
 import br.com.fiap.ffw.techffw.exception.DBException;
 import br.com.fiap.ffw.techffw.model.Usuario;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OracleUsuarioDao implements UsuarioDao {
@@ -26,8 +25,11 @@ public class OracleUsuarioDao implements UsuarioDao {
             stmt.setString(3, usuario.getSenha());
             stmt.setString(4, usuario.getCpf());
             stmt.executeUpdate();
+
+            System.out.println("Usuário registrado com sucesso");
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DBException(e);
         } finally {
             try{
                 stmt.close();
@@ -40,21 +42,139 @@ public class OracleUsuarioDao implements UsuarioDao {
 
     @Override
     public void atualizar(Usuario usuario) throws DBException {
+        PreparedStatement stmt = null;
 
+        Connection connection = ConnectionManager.getInstance().getConnection();
+        String sql = "UPDATE T_FFW_USUARIO SET "+
+                "nome_completo = ?, login = ?, senha = ?, cpf = ? WHERE cod_usuario = ?";
+
+        try{
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getLogin());
+            stmt.setString(3, usuario.getSenha());
+            stmt.setString(4, usuario.getCpf());
+            stmt.setInt(5, usuario.getId());
+            stmt.executeUpdate();
+
+            System.out.println("Usuário atualizado com sucesso");
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new DBException("Erro ao atualizar usuario.");
+        } finally {
+            try{
+                stmt.close();
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
-    public void remover(Usuario usuario) throws DBException {
+    public void remover(int id) throws DBException {
+        PreparedStatement stmt = null;
+
+        Connection connection = ConnectionManager.getInstance().getConnection();
+        String sql = "DELETE FROM T_FFW_USUARIO WHERE cod_usuario = ?";
+
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            System.out.println("Usuário removido com sucesso");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DBException("Erro ao remover usuario.");
+        } finally {
+            try{
+                stmt.close();
+                connection.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
     @Override
     public Usuario buscarPorNome(String nome) {
-        return null;
+        Usuario user = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        Connection connection = ConnectionManager.getInstance().getConnection();
+
+        try{
+            String sql = "SELECT * FROM T_FFW_USUARIO WHERE nome_completo = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, nome);
+            rs = stmt.executeQuery();
+
+            if(rs.next()){
+                int id = rs.getInt("cod_usuario");
+                String nomeCompleto = rs.getString("nome_completo");
+                String login = rs.getString("login");
+                String senha = rs.getString("senha");
+                String cpf = rs.getString("cpf");
+                user = new Usuario(id, nomeCompleto, login, senha, cpf);
+                System.out.println("Usuário encontrado com sucesso");
+
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                stmt.close();
+                rs.close();
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return user;
     }
 
     @Override
     public List<Usuario> listar() {
-        return List.of();
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        Connection connection = ConnectionManager.getInstance().getConnection();
+
+        try{
+            String sql = "SELECT * FROM T_FFW_USUARIO";
+            stmt = connection.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            System.out.println("Busca realizada com sucesso");
+
+
+            while (rs.next()){
+                int id = rs.getInt("cod_usuario");
+                String nomeCompleto = rs.getString("nome_completo");
+                String login = rs.getString("login");
+                String senha = rs.getString("senha");
+                String cpf = rs.getString("cpf");
+                Usuario usuario = new Usuario(id, nomeCompleto, login, senha, cpf);
+                usuarios.add(usuario);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                stmt.close();
+                rs.close();
+                connection.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return usuarios;
     }
 }
---
+++ src/main/java/br/com/fiap/ffw/techffw/test/UsuarioDaoTest.java
@@ -0,0 +1,59 @@
+package br.com.fiap.ffw.techffw.test;
+
+import br.com.fiap.ffw.techffw.dao.UsuarioDao;
+import br.com.fiap.ffw.techffw.exception.DBException;
+import br.com.fiap.ffw.techffw.factory.DaoFactory;
+import br.com.fiap.ffw.techffw.model.Usuario;
+
+import java.util.List;
+
+public class UsuarioDaoTest {
+
+    public static void main(String[] args) {
+
+        //Cadastrar
+        UsuarioDao dao = DaoFactory.getUsuarioDao();
+
+        Usuario user = new Usuario(
+                0,
+                "user",
+                "user",
+                "user",
+                "123456789"
+        );
+
+//        try {
+//            dao.cadastrar(user);
+//        } catch (DBException e) {
+//            throw new RuntimeException(e);
+//        }
+
+
+        //Buscar e atualizar
+//        user = dao.buscarPorNome(user.getNome());
+//        user.setNome("pedro");
+//        user.setCpf("98765432100");
+
+//        try {
+//            dao.atualizar(user);
+//        }catch (DBException e) {
+//            e.printStackTrace();
+//        }
+        //remover
+//        try {
+//            dao.remover(3);
+//        } catch (DBException e) {
+//            e.printStackTrace();
+//        }
+
+//        //listar
+//        List<Usuario> usuarios = dao.listar();
+//        for (Usuario usuario : usuarios) {
+//            System.out.println("Nome: " + usuario.getNome()+ " login: " + usuario.getLogin() +
+//                    " senha: " + usuario.getSenha() + " cpf: " + usuario.getCpf()
+//            );
+//        }
+
+
+    }
+}
