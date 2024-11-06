b6d819484438ce84619a27fa07efa98f56e87324
pedromarquesini <pedromarquesini1410@gmail.com>
2024-11-05 23:15:17 -0300

    add: create Usuario

---
+++ src/main/java/br/com/fiap/ffw/techffw/dao/impl/OracleUsuarioDao.java
@@ -0,0 +1,59 @@
+package br.com.fiap.ffw.techffw.dao.impl;
+
+import br.com.fiap.ffw.techffw.dao.ConnectionManager;
+import br.com.fiap.ffw.techffw.dao.UsuarioDao;
+import br.com.fiap.ffw.techffw.exception.DBException;
+import br.com.fiap.ffw.techffw.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class OracleUsuarioDao implements UsuarioDao {
+
+    @Override
+    public void cadastrar(Usuario usuario) throws DBException {
+        PreparedStatement stmt = null;
+
+        Connection connection = ConnectionManager.getInstance().getConnection();
+        String sql = "INSERT INTO T_FFW_USUARIO (cod_usuario, nome_usuario, login, senha) VALUES (SQ_TB_USUARIO.NEXTVAL,?,?,?)";
+
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getLogin());
+            stmt.setString(3, usuario.getSenha());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                stmt.close();
+                connection.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void atualizar(Usuario usuario) throws DBException {
+
+    }
+
+    @Override
+    public void remover(Usuario usuario) throws DBException {
+
+    }
+
+    @Override
+    public Usuario buscarPorNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public List<Usuario> listar() {
+        return List.of();
+    }
+}
