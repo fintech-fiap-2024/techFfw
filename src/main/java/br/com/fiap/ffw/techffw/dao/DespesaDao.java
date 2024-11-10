c5c05a50597dba6a6422bcbcffc51e4f974a2737
Geovani Carlos de Souza <geovanicsouza7@gmail.com>
2024-11-10 18:03:23 -0300

    trabalhando no investimento

--- src/main/java/br/com/fiap/ffw/techffw/dao/DespesaDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.ffw.techffw.dao;
-
-import br.com.fiap.ffw.techffw.exception.DBException;
-import br.com.fiap.ffw.techffw.model.Despesa;
-
-public interface DespesaDao {
-    void criarDespesa(Despesa despesa) throws DBException;
-    Despesa buscarDespesa(int id) throws DBException;
-}
--- src/main/java/br/com/fiap/ffw/techffw/factory/DaoFactory.java
+++ src/main/java/br/com/fiap/ffw/techffw/factory/DaoFactory.java
@@ -7,9 +7,6 @@ public class DaoFactory {
     public static UsuarioDao getUsuarioDao() {
         return new OracleUsuarioDao();
     }
-    public static ReceitaDao getReceitaDao() {
-        return new OracleReceitaDao();
-    }
     public static ObjetivoFinanceiroDao getObjetivoFinanceiroDao() { return new OracleObjetivoFinanceiroDAO();}
     public static TransacaoDao getTransacaoDao() { return new OracleTransacaoDao();}
     public static InvestimentoDao getInvestimentoDao(){return new OracleInvestimentoDao();}
